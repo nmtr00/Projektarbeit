2e84223a9968ed637fdbfbced0f426044051578e
caberfan <caberfan@gmail.com>
2024-04-22 00:19:10 +0200

    use methods instead of direct big loop

--- main.py
+++ main.py
@@ -9,29 +9,37 @@ from video_format import video_format
 input_folder = "O:\Projektarbeit\Converted to Mask with ImageJ"
 output_folder = "O:\Projektarbeit\Analyzed"
 pixels_per_mm =30
-x_correction = 10
+x_correction = 37
 min_area1 = 5000
 
+def video_format(video, name, output):
+    cap = cv2.VideoCapture(video)  # Replace 'your_video_file.mp4' with your video file path
+
+    # Check if the video file opened successfully
+    if not cap.isOpened():
+        print("Error: Could not open video file.")
+        exit()
+
+    # Get video properties
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(f'{output}\ANALYZED_{name}', fourcc, fps, (frame_width, frame_height))
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    return out, cap, total_frames
 
 
-
-# Iterate over all files in the folder
-for filename in os.listdir(input_folder):
-    if filename.endswith(".avi") or filename.endswith(".mp4"):  # Add more video formats if needed
-        video_path = os.path.join(input_folder, filename)
-        print("Processing video:", filename)
-
-
+def process_videos(video_path):
     # Load the video file
     out, cap, total_frames = video_format(video_path, filename, output_folder)
-
-
+     # Get total number of frames in the video
+    frame_count = 0
     # Define cropping parameters (x, y, width, height)
     x1, y1, w1, h1 = 75, 600, 1750, 180
     x2, y2, w2, h2 = 925, 925, 50, 275
-     # Get total number of frames in the video
-    
-    frame_count = 0
     while True:
         #Read a frame from the vieeo
         ret, frame = cap.read()
@@ -51,7 +59,6 @@ for filename in os.listdir(input_folder):
         analyzer_width = ContourAnalyzer() # Instantiate ContourAnalyzer
         contours_idx = analyzer_width.get_contour_indices(cropped_frame1, contours_width)   # Get contour indices 
         # print(contours_idx)
-
 
         if len(contours_idx)>1:
             # Identify left and right contours
@@ -86,3 +93,9 @@ for filename in os.listdir(input_folder):
     cap.release()
     out.release()
     cv2.destroyAllWindows()
+
+for filename in os.listdir(input_folder):
+    if filename.endswith(".avi") or filename.endswith(".mp4"):  # Add more video formats if needed
+        video_path = os.path.join(input_folder, filename)
+        print("Processing video:", filename)
+        process_videos(video_path)
