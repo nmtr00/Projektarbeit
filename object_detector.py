d6cd76054118c0313744c0b2853a01dc9482ac4f
caberfan <caberfan@gmail.com>
2024-04-30 11:24:44 +0200

    now the script works without being  converted to mask before running

--- object_detector.py
+++ object_detector.py
@@ -2,13 +2,17 @@ import cv2
 class HomogeneousBgDetector():
     def __init__(self):
         pass
-    def detect_object(self, frame, min_area):
+    def detect_object(self, frame, min_area, mask_min, mask_max):
         #Convert to gray scale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
 
         #Create a Mask with adaptive threshold
-        mask = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 19, 21)     
+        # mask = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 19, 21)  
+        mask = cv2.inRange(gray, mask_min, mask_max)   
+        # cv2.imshow("gray", gray)
         # cv2.imshow("mask", mask)
+        
         #Find contours
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
