3c91f21a5e2026d3f980221b64fd1e25eb6b6e02
caberfan <caberfan@gmail.com>
2024-06-12 16:41:05 +0200

    parametric study abaqus

---
+++ Abaqus/Parametric_study/HPC_uploaded/parametric_study.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/zsh
+
+### Start of Slurm SBATCH definitions
+### Maximum runtime
+#SBATCH --time=8:00:00
+
+### Ask for --ntask cpus (tasks in Slurm terms) across nodes
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+
+#ask for 1GB within each node (1)
+#SBATCH --mem-per-cpu=512M
+
+### Name the job
+#SBATCH --job-name=projektarbeit_neo_hook_1
+
+### Declare an output STDOUT/STDERR file
+#SBATCH --output=output.%J.txt
+
+### Choose Partition
+#SBATCH -p c23ms_low
+
+
+######################################################
+### Load required modules
+module load ABAQUS/2022
+
+### Set the amount of memory to be passed to Abaqus as a command line argument
+### Beware: This HAS to be lower than the total memory you requested.
+### A good starting point is 80%.
+export ABAQUS_MEM_ARG="3584 mb"
+
+
+### name your job HERE, name it DIFFERENT from your input file!
+JOBNAME=neo_hook_1
+INPUTFILE=curve_test.psf
+
+
+### Execute your application
+### Please remember, to adjust the memory, it must be less than the total requested memory
+abaqus script=curve_test.psf > curve_test.log
+mkdir -p results
+mv *.odb results/
+cd ..
+mv Curve_test /hpcwork/ys271564/Projektarbeit
